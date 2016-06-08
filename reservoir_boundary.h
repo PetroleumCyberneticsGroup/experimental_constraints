f271c2723d6ef689ec715db00c1ac9999bcbed24
Einar Baumann <einar.baumann@gmail.com>
2016-06-08 13:51:49 +0200

    Removed VariablePropertyHandler

--- reservoir_boundary.h
+++ reservoir_boundary.h
@@ -6,34 +6,35 @@
 #include "Optimization/constraints/well_spline_constraint.h"
 #include "Model/reservoir/grid/grid.h"
 
-namespace Optimization { namespace Constraints {
+namespace Optimization {
+    namespace Constraints {
 
-/*!
- * \brief The ReservoirBoundary class deals with imposing and checking reservoir boundary constraints
- *
- *  The class takes a boundary as input in the form of i,j and k min,max, i.e. a 'box' in
- *  the grid i,j,k coordinates. It can check wether or not a single well is inside the
- *  given box domain and, if needed, project the well onto the domain.
- */
-class ReservoirBoundary : public Constraint, WellSplineConstraint
-{
-public:
-    ReservoirBoundary(const Utilities::Settings::Optimizer::Constraint &settings,
-                          Model::Properties::VariablePropertyContainer *variables, Model::Reservoir::Grid::Grid *grid);
+        /*!
+         * \brief The ReservoirBoundary class deals with imposing and checking reservoir boundary constraints
+         *
+         *  The class takes a boundary as input in the form of i,j and k min,max, i.e. a 'box' in
+         *  the grid i,j,k coordinates. It can check wether or not a single well is inside the
+         *  given box domain and, if needed, project the well onto the domain.
+         */
+        class ReservoirBoundary : public Constraint, WellSplineConstraint
+        {
+        public:
+            ReservoirBoundary(const Utilities::Settings::Optimizer::Constraint &settings,
+                              Model::Properties::VariablePropertyContainer *variables, Model::Reservoir::Grid::Grid *grid);
 
-    // Constraint interface
-public:
-    bool CaseSatisfiesConstraint(Case *c);
-    void SnapCaseToConstraints(Case *c);
+            // Constraint interface
+        public:
+            bool CaseSatisfiesConstraint(Case *c);
+            void SnapCaseToConstraints(Case *c);
 
-private:
-    int imin_, imax_, jmin_, jmax_, kmin_, kmax_;
-    QList<int> index_list_;
-    Model::Reservoir::Grid::Grid *grid_;
-    Well affected_well_;
-    QList<int> getListOfCellIndices();
+        private:
+            int imin_, imax_, jmin_, jmax_, kmin_, kmax_;
+            QList<int> index_list_;
+            Model::Reservoir::Grid::Grid *grid_;
+            Well affected_well_;
+            QList<int> getListOfCellIndices();
 
-};
+        };
     }
 }
 
--- tests/test_reservoir_boundary.cpp
+++ tests/test_reservoir_boundary.cpp
@@ -11,26 +11,15 @@ namespace {
                                   public TestResources::TestResourceGrids,
                                   public TestResources::TestResourceSettings {
 
-
     public:
         ReservoirBoundaryTest() { }
-
-    public:
         virtual ~ReservoirBoundaryTest() { }
-
-        virtual void TearDown() {
-
-        }
-
-        virtual void SetUp() {
-
-        }
+        virtual void TearDown() { }
+        virtual void SetUp() { }
     };
 
     TEST_F(ReservoirBoundaryTest, Initialization) {
-
         auto test_boundary = Optimization::Constraints::ReservoirBoundary(constraint_settings_reservoir_boundary_, variable_property_container_, grid_5spot_);
-
         EXPECT_FALSE(test_boundary.CaseSatisfiesConstraint(test_case_5_));
         test_boundary.SnapCaseToConstraints(test_case_5_);
         EXPECT_TRUE(test_boundary.CaseSatisfiesConstraint(test_case_5_));
